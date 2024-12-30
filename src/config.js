373d3cd3e8e89f28745d871d44d0b4082c171461
Noah Loomis <noahmmloomis@gmail.com>
2024-12-30 11:13:03 -0500

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  siteTitle: "Don't Get Sunburnt",
+  siteTagline: 'Hiking the Pacific Crest Trail',
+  archiveTitle: 'Archive',
+  dateFormat: 'MMM dd, yyyy',
+};
+
+export default config;
